ae26921b814c37fce732664106963cd1842f3ad6
Carver Harrison <pcarverb@hotmail.com>
2016-12-28 22:22:09 -0800

    Create blog.js

---
+++ blog.js
@@ -0,0 +1,7 @@
+var app = angular.module("app", []);
+app.controller("head", function($scope) {
+});
+app.controller("navbar", function($scope) {
+});
+app.controller("body", function($scope) {
+});
