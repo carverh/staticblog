4965538c3153ba6bd034ecb8a43df7372653dbca
Carver Harrison <pcarverb@hotmail.com>
2016-12-28 22:41:06 -0800

    Update blog.js

--- blog.js
+++ blog.js
@@ -3,8 +3,11 @@ $.get("blog.xml", function (data) {
   blogData = data;
 });
 
+x = $(blogData);
+
 var app = angular.module("app", []);
 app.controller("head", function($scope) {
+  $scope.title = x.find("head title")
 });
 app.controller("navbar", function($scope) {
 });
