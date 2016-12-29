612bc52ad20cad9a5cc58c120150c2591758f151
Carver Harrison <pcarverb@hotmail.com>
2016-12-28 23:31:00 -0800

    Update blog.js

--- blog.js
+++ blog.js
@@ -15,13 +15,14 @@ app.controller("head", function($scope) {
 
 app.controller("navbar", function($scope) {
   $scope.title = $(blogData).find("navbar title").text();
-  $scope.navitems = []
+  $scope.navitems = [];
   $(blogData).find("navbar items item").each(function (a, b) {
     $scope.navitems.push({name: $(this).find("name").text(), url: $(this).find("url").text()});
   });
 });
 
 app.controller("body", function($scope) {
+  $scope.posts = [];
   $(blogData).find("posts post").each(function (a, b) {
     $scope.posts.push({id: $(this).find("id").text(), title: $(this).find("title").text(), body: $(this).find("body").text(), author: $(this).find("author").text(), tags: $(this).find("tags").text().split(',')});
   });
