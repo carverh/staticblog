b805197359d2fe4c0e486438c1761c59addec9c1
Carver Harrison <pcarverb@hotmail.com>
2016-12-28 23:03:50 -0800

    Update blog.js

--- blog.js
+++ blog.js
@@ -16,7 +16,7 @@ app.controller("head", function($scope) {
 app.controller("navbar", function($scope) {
   $scope.title = $(blogData).find("navbar title").text();
   $scope.items = []
-  $(blogData).find("navbar items").each(function (a, b) {
+  $(blogData).find("navbar items item").each(function (a, b) {
     $scope.items.push({name: this.find("name").text(), url: this.find("url").text()});
   });
 });
