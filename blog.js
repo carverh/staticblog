fdf9dcc670ea1cd072bd23226c254780b4b18991
Carver Harrison <pcarverb@hotmail.com>
2016-12-28 23:30:21 -0800

    Update blog.js

--- blog.js
+++ blog.js
@@ -22,7 +22,7 @@ app.controller("navbar", function($scope) {
 });
 
 app.controller("body", function($scope) {
-  $(blogData).find("body posts post").each(function (a, b) {
+  $(blogData).find("posts post").each(function (a, b) {
     $scope.posts.push({id: $(this).find("id").text(), title: $(this).find("title").text(), body: $(this).find("body").text(), author: $(this).find("author").text(), tags: $(this).find("tags").text().split(',')});
   });
 });
